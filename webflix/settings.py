50c95d6b7437405805085ff81e2cf7166d2e4545
majerhua <majerhua@gmail.com>
2017-10-18 05:39:44 -0500

    tercera subida master

--- webflix/settings.py
+++ webflix/settings.py
@@ -125,10 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 
-STATICFILES_DIRS = (
-       os.path.join(BASE_DIR, 'media'),
-   )
-
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
