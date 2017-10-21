45d688fb7fc28f3993e3b95ac368a7e9aa99a6ed
majerhua <majerhua@gmail.com>
2017-10-21 17:23:00 -0500

    mod

--- series/admin.py
+++ series/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Proyecto
-
+from .models import ProyectoDocs
 
 
 admin.site.register(Proyecto)
+admin.site.register(ProyectoDocs)
